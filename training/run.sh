4dcf60991e85beb8538c23a239ac4ff60a3f7bd5
hoangvu <phamlehoangvu97@gmail.com>
2024-08-20 06:12:25 +0000

    modified:   data/synthetic-pii-ner.json.dvc 	modified:   notebooks/finetune.ipynb 	new file:   training/fine_tune.py 	new file:   training/run.sh

---
+++ training/run.sh
@@ -0,0 +1,20 @@
+python fine_tune.py \
+  --model_name_or_path ../models/gliner_medium-v2.1 \
+  --data_path ../data/synthetic-pii-ner.json \
+  --output_dir ../models/gliner_medium-v2.1 \
+  --learning_rate 5e-6 \
+  --weight_decay 0.01 \
+  --others_lr 1e-5 \
+  --others_weight_decay 0.01 \
+  --lr_scheduler_type "linear" \
+  --warmup_ratio 0.1 \
+  --per_device_train_batch_size 16 \
+  --per_device_eval_batch_size 16 \
+  --num_train_epochs 1 \
+  --evaluation_strategy "steps" \
+  --save_steps 500 \
+  --save_total_limit 10 \
+  --dataloader_num_workers 0 \
+  --use_cpu False \
+  --report_to "wandb" \
+  --logging_steps 1
