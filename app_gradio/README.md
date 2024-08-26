dd205fc6295523e2022e0801df77daf45dbc0b55
hoangvu <phamlehoangvu97@gmail.com>
2024-08-26 18:17:01 +0700

    new file:   app_gradio/README.md 	modified:   app_gradio/app.py

---
+++ app_gradio/README.md
@@ -0,0 +1,4 @@
+## Zero-shot Named Entity Recognition
+
+For more on how this application works,
+[check out the GitHub repo](https://github.com/hoangvu971/NER-project).
--- app_gradio/app.py
+++ app_gradio/app.py
@@ -2,6 +2,7 @@
 import bentoml
 import gradio as gr
 
+README = "README.md"
 
 def extract_info(input_text, labels):
     with bentoml.SyncHTTPClient("http://localhost:3000") as client:
@@ -14,6 +15,14 @@ def process_input(input_text, labels_input):
     labels = [label.strip() for label in labels_input.split(",")]
     return extract_info(input_text, labels)
 
+def _load_readme():
+    with open(README) as f:
+        lines = f.readlines()
+        readme = "".join(lines)
+    return readme
+
+readme = _load_readme()
+examples = [["Libretto by Marius Petipa, based on the 1822 novella ``Trilby, ou Le Lutin d'Argail`` by Charles Nodier, first presented by the Ballet of the Moscow Imperial Bolshoi Theatre on January 25/February 6 (Julian/Gregorian calendar dates), 1870, in Moscow with Polina Karpakova as Trilby and Ludiia Geiten as Miranda and restaged by Petipa for the Imperial Ballet at the Imperial Bolshoi Kamenny Theatre on January 17–29, 1871 in St. Petersburg with Adèle Grantzow as Trilby and Lev Ivanov as Count Leopold." , "person, book, location, date, actor, character"]]
 
 # Create the Gradio interface
 iface = gr.Interface(
@@ -22,6 +31,8 @@ iface = gr.Interface(
     outputs=gr.JSON(label="Extracted Information"),
     title="Named Entity Extractor",
     description="Enter text and labels to extract entities.",
+    article=readme,
+    examples=examples
 )
 
 # Launch the app
